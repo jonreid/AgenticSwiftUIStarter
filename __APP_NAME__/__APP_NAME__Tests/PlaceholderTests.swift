3cfa1e22fe6a79a3c6f123e44efb810f8226cf3b
Jon Reid <jon@qualitycoding.org>
2026-08-16 21:33:39 -0700

    Add Placeholder test suite

---
+++ __APP_NAME__/__APP_NAME__Tests/PlaceholderTests.swift
@@ -0,0 +1,9 @@
+@testable import __APP_NAME__
+import Testing
+
+final class PlaceholderTests: @unchecked Sendable {
+    @Test
+    func zero() async throws {
+        Issue.record("Tests not yet implemented in PlaceholderTests")
+    }
+}
