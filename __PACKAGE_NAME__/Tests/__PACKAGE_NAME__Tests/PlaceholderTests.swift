555d074a438f141c62fa914aa9e00ed61dc3e5be
Jon Reid <jon@qualitycoding.org>
2026-08-16 22:07:57 -0700

    Add test_core.sh and make placeholder pass

--- __PACKAGE_NAME__/Tests/__PACKAGE_NAME__Tests/PlaceholderTests.swift
+++ __PACKAGE_NAME__/Tests/__PACKAGE_NAME__Tests/PlaceholderTests.swift
@@ -3,7 +3,6 @@ import Testing
 
 final class PlaceholderTests: @unchecked Sendable {
     @Test
-    func zero() async throws {
-        Issue.record("Tests not yet implemented in PlaceholderTests")
+    func success() async throws {
     }
 }
