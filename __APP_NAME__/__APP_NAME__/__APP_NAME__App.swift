8c7b0aef4d2dc72fe187471294181d30f0a335c0
Jon Reid <jon@qualitycoding.org>
2026-08-16 18:15:55 -0700

    Initial commit: Xcode new project, iOS app
    https://qualitycoding.org/generate-xcode-gitignore/

---
+++ __APP_NAME__/__APP_NAME__/__APP_NAME__App.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct __APP_NAME__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ __APP_NAME__/__APP_NAME__Tests/__APP_NAME__Tests.swift
@@ -0,0 +1,14 @@
+//
+
+import Testing
+@testable import __APP_NAME__
+
+struct __APP_NAME__Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
