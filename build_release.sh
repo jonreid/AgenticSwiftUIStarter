b2099af561111ff0e45b2923ef55a3d8c25dc4c2
Jon Reid <jon@qualitycoding.org>
2026-08-29 17:05:38 -0700

    Add build_release.sh

---
+++ build_release.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCHEME='AppNamePlaceholder'
+DESTINATION='platform=iOS Simulator,OS=26.4.1,name=iPhone 17'
+
+xcodebuild -configuration Release -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify
